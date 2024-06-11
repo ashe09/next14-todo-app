3a16854d793243702bbe0bd404c66c42f7eba226
ashe09 <creation.oasis21la@gmail.com>
2024-06-12 00:35:44 +0900

    カードのdrop-shadowのカスタムクラスを追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,6 +66,9 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out"
+      },
+      dropShadow: {
+        "2xl-white": "0 25px 25px rgb(255 255 255 / 0.15)"
       }
     }
   },
